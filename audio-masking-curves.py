995b8bc618dcd2d1cb0faa3b0b0e0659560ea7d0
Gabriella Felgas <gabriellafelgas@poli.ufrj.br>
2019-07-05 17:07:28 -0300

    added details

--- audio-masking-curves.py
+++ audio-masking-curves.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# UFRJ/POLI/DEL - Processamento de Sinais de Audio - 2019.1
+# Professor: Luiz Wagner Pereira Biscainho
+# Exercicio 19 - Curvas de Mascaramento
+# Grupo 1: Maria Gabriella Felgas, Matheus Moreno e Miguel de Sousa
+  
 from scipy import signal
 from scipy.io import wavfile
 
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+
+# In[2]:
+
 
 # Definindo as variaveis iniciais
 
@@ -34,46 +49,66 @@ freqmin = 0
 freqhop = (freqmax - freqmin)/nfft
 
 # O sinal de teste, x, eh um combinacao de cosenoides de diferentes periodos
-x = np.multiply(A0, np.cos(np.multiply(2*(np.pi)*f0/fs, n))) + np.multiply(A1, np.cos(np.multiply(2*(np.pi)*f1/fs, n))) + np.multiply(A2, np.cos(np.multiply(2*(np.pi)*f2/fs, n))) + np.multiply(A3, np.cos(np.multiply(2*(np.pi)*f3/fs, n))) + np.multiply(A4, np.cos(np.multiply(2*(np.pi)*f4/fs, n))) + np.multiply(A5, np.cos(np.multiply(2*(np.pi)*f5/fs, n)))
+x = np.multiply(A0, np.cos(np.multiply(2*(np.pi)*f0/fs, n))) + np.multiply(A1, np.cos(np.multiply(2*(np.pi)*f1/fs, n)))     + np.multiply(A2, np.cos(np.multiply(2*(np.pi)*f2/fs, n))) + np.multiply(A3, np.cos(np.multiply(2*(np.pi)*f3/fs, n)))     + np.multiply(A4, np.cos(np.multiply(2*(np.pi)*f4/fs, n))) + np.multiply(A5, np.cos(np.multiply(2*(np.pi)*f5/fs, n)))
 
+plt.figure(figsize=(20,10))
 plt.plot(n, x)
 plt.ylabel('x[n]')
 plt.xlabel('n')
 plt.show()
 
-# Realizamos a fft de x e avaliamos seus valores absolutos
-x_fft = np.fft.fft(x)
-x_fft_2 = abs(x_fft) ** 2
+
+# In[3]:
+
+
+# Realizamos a FFT de x e avaliamos seus valores absolutos
 
 freq = np.dot(n, freqhop)
 
-plt.plot(freq, x_fft_2)
-plt.ylabel('(X[k])²')
+x_fft = np.fft.fft(x)
+
+plt.figure(figsize=(20,10))
+plt.plot(freq, abs(x_fft))
+plt.ylabel('|X[k]|')
 plt.xlabel('Frequency (Hz)')
 plt.show()
+
+
+# Depois, realizamos o calculo do modulo ao quadrado da FFT
+
+x_fft_2 = abs(x_fft) ** 2
+
+plt.figure(figsize=(20,10))
+plt.plot(freq, x_fft_2)
+plt.ylabel('|X[k]|²')
+plt.xlabel('Frequency (Hz)')
+plt.show()
+
+
+# In[4]:
+
 
 # Encontramos os picos da FFT
 peaks, _ = signal.find_peaks(x_fft_2)
 
 
 # Definimos a funcao de transformacao de dominios da frequencia (FFT)
-# para dominio Bark  e vice-versa (utilizamos a formula que parecia mais
-# atualizada de acordo com os artigos recentes sobre o assunto)
+# para dominio Bark  e vice-versa (utilizamos a aproximacao de
+# Wang, Sekey, Gersho (1992))
 
 def freq2bark(freq):
 
-    bark = 6 * np.arcsinh(freq/600)
+    bark = 6 * np.arcsinh(freq / 600)
     
     return bark
 
 def bark2freq(bark):
 
-    freq = 650*np.sinh(bark/7)
+    freq = 650 * np.sinh(bark / 7)
     return freq
 
 
-# Definimos a funcao de transformacao da escala linear para a
-# escala SPL em dB
+# Definimos a funcao de transformacao para SPL, em dB
 
 def amp2spl(x_fft_2):
     
@@ -83,25 +118,39 @@ def amp2spl(x_fft_2):
     
     return SPL
 
+
+# In[5]:
+
+
 # Realizamos a transformacao de dominios
 
 bark = freq2bark(freq)
 
+plt.figure(figsize=(20,10))
 plt.plot(bark, x_fft_2)
-plt.ylabel('(X[k])²')
+plt.ylabel('|X[k]|²')
 plt.xlabel('Frequency (Bark)')
 plt.show()
+
+
+# In[6]:
+
 
 # E a transformacao de escalas
 
 SPL = amp2spl(x_fft_2)
 
 
+plt.figure(figsize=(20,10))
 plt.plot(bark, SPL)
 plt.plot(bark[peaks], SPL[peaks], marker ='o', ls='')
 plt.ylabel('SPL (dB)')
 plt.xlabel('Frequency (Bark)')
 plt.show()
+
+
+# In[7]:
+
 
 # Definimos as spreading functions
 
@@ -129,11 +178,19 @@ def spreading_function(bark, SPL, peaks, down_shift):
     return sf_peaks
 
 
+# In[8]:
+
+
 # E aplicamos aos picos de acordo com seus espalhamentos
 
 sf_peaks_1 = spreading_function(bark, SPL, peaks, down_shift=0)
 sf_peaks_2 = spreading_function(bark, SPL, peaks, down_shift=16)
 
+
+# Ajustamos as spreading functions para serem
+# desconsideradas a partir do momento que apresentam
+# valores negativos, setando nosso limite inferior
+# em 0 dB
 
 for i in peaks:
     
@@ -146,6 +203,9 @@ for i in peaks:
         if sf_peaks_2[i][j] < 0:
             
             sf_peaks_2[i][j] = 0
+
+
+# In[9]:
 
 
 # Desconsiderando o delta
@@ -166,6 +226,10 @@ plt.ylabel('SPL (dB)')
 plt.xlabel('Frequency (Bark)')
 plt.show()
 
+
+# In[10]:
+
+
 # Considerando delta = 16 dB
 
 print('Down-shift = 16 dB: ')
@@ -184,6 +248,10 @@ plt.ylabel('SPL (dB)')
 plt.xlabel('Frequency (Bark)')
 plt.show()
 
+
+# In[11]:
+
+
 # Considerando alfa tendendo a infinito,
 # nossa curva de mascaramento eh definida
 # pelas spreading functions de maior valor de cada ponto
@@ -198,6 +266,10 @@ def mask_curve(bark, sf_peaks):
         
     return mask
 
+
+# In[12]:
+
+
 mask_1 = mask_curve(bark, sf_peaks_1)
 
 plt.figure(figsize=(20,10))
@@ -207,6 +279,10 @@ plt.plot(bark[peaks], SPL[peaks], marker ='o', ls='')
 plt.ylabel('SPL (dB)')
 plt.xlabel('Frequency (Bark)')
 plt.show()
+
+
+# In[13]:
+
 
 # Como pode ser observado no grafico acima,
 # algumas das frequencias serao mascaradas
@@ -230,6 +306,9 @@ print('Frequências mascaradas: ', np.round(masked_1, 1))
 print('Frequências não mascaradas: ', np.round(non_masked_1, 1))
 
 
+# In[14]:
+
+
 mask_2 = mask_curve(bark, sf_peaks_2)
 
 plt.figure(figsize=(20,10))
@@ -239,6 +318,10 @@ plt.plot(bark[peaks], SPL[peaks], marker ='o', ls='')
 plt.ylabel('SPL (dB)')
 plt.xlabel('Frequency (Bark)')
 plt.show()
+
+
+# In[15]:
+
 
 # Como pode ser observado no grafico acima,
 # algumas das frequencias serao mascaradas
@@ -260,3 +343,4 @@ for i in peaks:
         
 print('Frequências mascaradas: ', np.round(masked_2, 1))
 print('Frequências não mascaradas: ', np.round(non_masked_2, 1))
+
